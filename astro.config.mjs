e5ab3c3243ff41d6b1453528edfd904b0dec798d
DamianDTM <DamianDTM@users.noreply.github.com>
2026-03-20 20:20:22 -0500

    Configurar adaptador Vercel y Node engine

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare()
+  adapter: vercel()
 });
